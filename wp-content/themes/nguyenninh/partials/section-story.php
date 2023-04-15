d550fa7a807d9250dc0e58fd135dbd270c4ff58e
ThanhLV <thanhlv@newwave.vn>
2023-04-15 09:39:31 +0700

    fix bug

--- wp-content/themes/nguyenninh/partials/section-story.php
+++ wp-content/themes/nguyenninh/partials/section-story.php
@@ -10,12 +10,12 @@ $array_defaults = array(
 
 $args = wp_parse_args($args, $array_defaults);
 ?>
-<section class="position-relative section-story title-block-common" style="background: url(<?php echo get_template_directory_uri(); ?>//assets/imgs/IMG_9840.jpg);">
+<section class="position-relative section-story title-block-common d-flex flex-row align-items-center" style="background: url(<?php echo get_template_directory_uri(); ?>//assets/imgs/IMG_9840.jpg);">
     <div class="overlay"></div>
     <div class="container z-3 position-relative">
-        <div class="d-flex flex-col align-items-center pt-96 pb-96 mx-[3rem]">
+        <div class="d-flex flex-col align-items-center pt-96 pb-96">
             <p class="sub-title text-white"><?= $args['sub-title'] ?></p>
-            <div class="title-main d-flex align-items-center text-center">
+            <div class="title-main d-flex align-items-center text-center w-75">
                 <p class="text-white"><?= $args['title'] ?></p>
             </div>
             <?php get_template_part('partials/btn', 'common', [
