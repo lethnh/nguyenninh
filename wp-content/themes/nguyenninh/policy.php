62ac66c2f2f98920f2e30d38609b01ba55f43c96
ThanhLV <thanhlv@newwave.vn>
2023-04-18 23:19:12 +0700

    fix bug

--- wp-content/themes/nguyenninh/policy.php
+++ wp-content/themes/nguyenninh/policy.php
@@ -9,7 +9,17 @@ get_header(); ?>
 <div class="border-[0.5px] border-[#e6e6e6] w-[calc(100%_-_8rem)] mx-auto mt-24"></div>
 <section class="section-common">
     <div class="container">
-
+        <h1>Chính sách Cookies</h1>
+        <h2>Sử dụng cookies</h2>
+        <div class="text-style-base">Một số nội dung của website của Nguyên Ninh sử dụng cookies để cung cấp dịch vụ tốt hơn cho các quý khách hàng.</div>
+        <h2>Cookies là gì?</h2>
+        <div class="text-style-base">Một số nội dung của website của Nguyên Ninh sử dụng cookies để cung cấp dịch vụ tốt hơn cho các quý khách hàng.</div>
+        <h2>Sử dụng cookies</h2>
+        <div class="text-style-base">A cookie is a small text file that is stored on your computer (or other Internet-enabled device such as a smartphone or tablet) when you access this website. By using cookies, we can obtain information such as the number of times your computer visits the website and the pages you visit. The information collected through cookies does not include “personal information that can identify the customer.”</div>
+        <div class="text-style-base">You can also disable the function of cookies by setting your browser. There is no problem in using this website even if the cookie function is disabled.</div>
+        <div class="text-style-base">This website uses cookies in some of its contents for the purposes listed below.</div>
+        <h2>Mục đích sử dụng</h2>
+        <div class="text-style-base">A cookie is a small text file that is stored on your computer (or other Internet-enabled device such as a smartphone or tablet) when you access this website. By using cookies, we can obtain information such as the number of times your computer visits the website and the pages you visit. The information collected through cookies does not include “personal information that can identify the customer.”</div>
     </div>
 </section>
 <?php get_footer(); ?>
