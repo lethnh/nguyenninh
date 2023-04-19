dc9f3a0f72d7e53d53a652fca12a2673bd2d4013
ThanhLV <thanhlv@newwave.vn>
2023-04-19 21:11:46 +0700

    fix bug

--- wp-content/themes/nguyenninh/single-post.php
+++ wp-content/themes/nguyenninh/single-post.php
@@ -13,11 +13,20 @@ get_header(); ?>
                 <div class="d-flex flex-col w-[45%] mr-20 align-self-center h-full title-block-common">
                     <p class="sub-title gray-80">Nhật ký</p>
                     <h1 class="post-title gray-80 mt-4"><?php the_title(); ?></h1>
+                    <p id="post_url" class="d-none"><?= get_the_permalink(); ?></p>
                     <p class="post-date gray-70">Đăng vào lúc <?php echo get_the_date('h:i') ?> | <?php echo get_the_date('d.m.Y') ?></p>
                     <div class="single_post_line"></div>
                 </div>
-                <div class="post-thumbnail w-50">
+                <div class="post-thumbnail w-50 position-relative">
                     <img class="h-100 object-cover w-100" src="<?php the_post_thumbnail_url() ?>">
+                    <div class="position-absolute d-flex flex-col bg-primary-40 post-social">
+                        <a href="javascript:void(0);" class="share-fb-or-twitter" data-url="<?= "https://www.facebook.com/sharer/sharer.php?u=" . get_the_permalink(); ?> rel=" nofollow noopener">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/imgs/facebook.svg" alt=""></a>
+                        <a href="javascript:void(0);" rel="nofollow noopener">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/imgs/instagram.svg" alt=""></a>
+                        <a href="javascript:void(0);" onclick="copyToClipboard('#post_url')" rel="nofollow noopener">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/imgs/link.svg" alt=""></a>
+                    </div>
                 </div>
             </div>
             <div class="section-post-content">
