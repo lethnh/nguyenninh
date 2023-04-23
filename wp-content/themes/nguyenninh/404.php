9b914a80143268fd535347c25fc1a8db90287607
ThanhLV <thanhlv@newwave.vn>
2023-04-23 18:27:01 +0700

    fix bug

--- wp-content/themes/nguyenninh/404.php
+++ wp-content/themes/nguyenninh/404.php
@@ -4,14 +4,14 @@ get_header(); ?>
 <section class="section-common section-404">
     <div class="container">
         <div class="badge-404 text-center">Error 404</div>
-        <div class="d-flex align-items-end">
+        <div class="d-flex align-items-end flex-col flex-md-row">
             <h1>Không tìm thấy trang <br>
                 bạn yêu cầu</h1>
             <?php get_template_part('partials/btn', 'common', [
                 'is_icon' => true,
                 'link' => get_home_url(),
                 'text' => 'Về Trang Chủ',
-                'class' => 'ms-auto'
+                'class' => 'ms-md-auto'
             ]); ?>
         </div>
     </div>
