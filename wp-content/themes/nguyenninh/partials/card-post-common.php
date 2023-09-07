157b01bdd92346cc7ce52084c441832eea0c9c72
thanhlv <levanthanh29111999@gmail.com>
2023-09-08 06:24:54 +0700

    fix bug

--- wp-content/themes/nguyenninh/partials/card-post-common.php
+++ wp-content/themes/nguyenninh/partials/card-post-common.php
@@ -11,20 +11,21 @@ $args = wp_parse_args($args, $array_defaults);
         <div class="img-wrapper"><?= get_the_post_thumbnail(get_the_ID(), 'post-thumbnail') ?></div>
     </a>
     <div class="post_body d-flex flex-col justify-content-between">
-        <div class="d-flex flex-col">
-            <div class="post_title gray-80 mb-3">
-                <a href="<?php the_permalink() ?>">
-                    <h2><?= wp_trim_words(the_title(), 18, '...'); ?></h2>
-                </a>
+        <a href="<?php the_permalink() ?>">
+            <div class="d-flex flex-col">
+                <div class="post_title gray-80 mb-3">
+                    <a href="<?php the_permalink() ?>">
+                        <h2><?= wp_trim_words(the_title(), 18, '...'); ?></h2>
+                    </a>
+                </div>
+                <div class="post_excerpt gray-60">
+                    <p><?= wp_trim_words(get_the_content(), 40, '...'); ?></p>
+                </div>
             </div>
-            <div class="post_excerpt gray-60">
-                <p><?= wp_trim_words(get_the_content(), 40, '...'); ?></p>
+            <div class="d-flex justify-content-between post_body_footer">
+                <div class="post_seemore gray-80">ĐỌC CÂU CHUYỆN<i class="fa-solid fa-arrow-up rotate-45"></i></div>
+                <div class="text-center gray-70 post_datetime"><?= get_the_date('d.m.Y') ?></div>
             </div>
-        </div>
-        <div class="d-flex justify-content-between post_body_footer">
-            <a href="<?php the_permalink() ?>" class="post_seemore gray-80">ĐỌC CÂU CHUYỆN
-                <i class="fa-solid fa-arrow-up rotate-45"></i></a>
-            <div class="text-center gray-70 post_datetime"><?= get_the_date('d.m.Y') ?></div>
-        </div>
+        </a>
     </div>
 </div>
