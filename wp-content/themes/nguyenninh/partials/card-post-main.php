8175b1f9d2ea2b977f20f24fdb0da6a1b17aa2af
thanhlv <levanthanh29111999@gmail.com>
2023-09-08 06:33:28 +0700

    fix bug

--- wp-content/themes/nguyenninh/partials/card-post-main.php
+++ wp-content/themes/nguyenninh/partials/card-post-main.php
@@ -34,18 +34,24 @@ $args = wp_parse_args($args, $array_defaults);
 <?php else : ?>
     <div class="position-relative col-span-2 post_item post_item_main <?= $args['class']; ?>">
         <div class="img-wrapper" style="<?= $args['style_image']; ?>">
-            <?= get_the_post_thumbnail($args['post_id'], 'post-thumbnail') ?>
+            <a href="<?php the_permalink($args['post_id']) ?>">
+                <?= get_the_post_thumbnail($args['post_id'], 'post-thumbnail') ?>
+            </a>
         </div>
         <?php if ($args['is_flag_story_of_week']) : ?>
-            <div class="position-absolute post_story_of_week bg-red-60 px-8 py-4 left-0 top-0 mt-0 text-white">Nổi bật tuần này</div>
+            <div class="position-absolute post_story_of_week bg-red-60 px-8 py-4 left-0 top-0 mt-0 text-white"><a href="<?php the_permalink($args['post_id']) ?>">Nổi bật tuần này</a></div>
         <?php endif; ?>
         <div class="position-absolute <?= $args['position_body']; ?> post_body d-flex flex-col justify-content-between">
             <div class="d-flex flex-col">
                 <div class="post_title gray-80 mb-3">
-                    <h2><?= wp_trim_words(get_the_title($args['post_id']), 18, '...'); ?></h2>
+                    <a href="<?php the_permalink($args['post_id']) ?>">
+                        <h2><?= wp_trim_words(get_the_title($args['post_id']), 18, '...'); ?></h2>
+                    </a>
                 </div>
                 <div class="post_excerpt gray-60">
-                    <p><?= wp_trim_words(get_the_content(null, false, $args['post_id']), 40, '...'); ?></p>
+                    <a href="<?php the_permalink($args['post_id']) ?>">
+                        <p><?= wp_trim_words(get_the_content(null, false, $args['post_id']), 40, '...'); ?></p>
+                    </a>
                 </div>
             </div>
             <div class="flex justify-content-between post_body_footer">
